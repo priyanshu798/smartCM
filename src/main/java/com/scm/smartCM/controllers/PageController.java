47a43162be0d0c0575376562f163624c317ecf2b
priyanshu798 <p.vishwakarma798@gmail.com>
2024-08-02 01:08:50 +0530

    add user validation

--- src/main/java/com/scm/smartCM/controllers/PageController.java
+++ src/main/java/com/scm/smartCM/controllers/PageController.java
@@ -1,21 +1,30 @@
 package com.scm.smartCM.controllers;
 
+import com.scm.smartCM.entities.User;
 import com.scm.smartCM.forms.UserForm;
 
 import com.scm.smartCM.helpers.Message;
 import com.scm.smartCM.helpers.MessageType;
+import com.scm.smartCM.services.UserService;
+
 import jakarta.servlet.http.HttpSession;
 
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class PageController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/home")
     public String home(Model model) {
@@ -25,8 +34,7 @@ public class PageController {
         return "home";
     }
 
-
-    //abbout route
+    // abbout route
     @RequestMapping("/about")
     public String aboutPage() {
         System.out.println("About page loading");
@@ -58,20 +66,40 @@ public class PageController {
         System.out.println("Register PAge");
         return "register";
     }
- 
-    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
-        System.out.println("regis ter");
-        //fetch form data
-        System.out.println(userForm);
-        //validate
 
-        //save to db
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult,
+            HttpSession session) {
+        System.out.println("Processing registration");
+        // fetch form data
+        // UserForm
+        System.out.println(userForm);
+
+        // validate form data
+        if (rBindingResult.hasErrors()) {
+            return "register";
+        }
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setEnabled(false);
+        user.setProfilePic(
+                "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75");
+
+        User savedUser = userService.saveUser(user);
+
+        System.out.println("user saved :");
+
         
-        //msg = reg success
-        Message message = Message.builder().content("Registration succesfull!").type(MessageType.blue).build();
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+
         session.setAttribute("message", message);
-        //regirect login page
+
+        // redirectto login page
         return "redirect:/register";
     }
 }
--- src/main/java/com/scm/smartCM/forms/UserForm.java
+++ src/main/java/com/scm/smartCM/forms/UserForm.java
@@ -1,9 +1,14 @@
 package com.scm.smartCM.forms;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Builder
 @NoArgsConstructor
@@ -12,10 +17,23 @@ import lombok.*;
 @Setter
 @ToString
 public class UserForm {
-    
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, message = "Min 3 characters are required")
     private String name;
+
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Min 6 characters are required")
     private String password;
+
+    @NotBlank(message = "About is required")
     private String about;
+    
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 8, max = 12, message = "Invalid Phone Number")
     private String phoneNumber;
 }
